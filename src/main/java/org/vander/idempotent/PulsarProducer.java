0a814ac5a66356d5b220b5a22ba04f8c4ef3ca9b
zhangzengmin <xiaomin0322@sina.com>
2020-05-27 11:53:04 +0800

    update

---
+++ src/main/java/org/vander/idempotent/PulsarProducer.java
@@ -0,0 +1,38 @@
+package org.vander.idempotent;
+
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+
+public class PulsarProducer {
+
+    private static PulsarClient client;
+    private static Producer<byte[]> producer;
+
+    public static void main(String[] args) throws Exception {
+        client = PulsarClient.builder()
+                .serviceUrl(org.vander.common.Config.URL)
+                .build();
+
+        producer = client.newProducer()
+                .topic("my-topic")
+                .create();
+
+        startProducer();
+
+    }
+
+    private static void startProducer() throws Exception {
+
+            System.out.println("Start produce");
+            producer.newMessage()
+                    .value("my-message-2".getBytes())
+                    .sequenceId(2)
+                    .send();
+
+
+            producer.newMessage()
+            .value("my-message-2".getBytes())
+            .sequenceId(2)
+            .send();
+    }
+}
